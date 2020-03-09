f9e22a788b6bca79548392395522efd6a8db4384
MindeurFou <tanguy.pouriel@utt.fr>
2020-03-09 19:48:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Donne-ton-avis
+ Survey website written for the course LO07 at the UTT
---
+++ public_html/test_javascript.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var episode = {
+   title: "salut",
+   duration: 60,
+   hasBeenWatched: true
+};
+
+var episodeTitle = episode.title;
+var episode  = episode.duration;
+
+class Book {
+    
+}
+ 
+
