a3fcb260979c4c8e295f7ef583a3d5c6c09848dd
MindeurFou <tanguy.pouriel@utt.fr>
2020-06-18 18:29:24 +0200

    Create api.php

---
+++ website_DTA/model/api.php
@@ -0,0 +1,36 @@
+<?php
+
+// pour récupérer un survey sous forme JSON :
+// faire une requete GET avec JQuery avec comme URI : api.php?idSurvey=x    avec x l'id du survey 
+
+header("Content-Type:application/json");
+require_once 'SurveyManager.php';
+
+if(!empty($_GET["idSurvey"])){
+    $idSurvey = htmlspecialchars($_GET["idSurvey"]);
+    
+    $survey = $surveyManager->getSurveyById($idSurvey);
+    
+    if(!empty($survey)){
+        $survey->setChoices($surveyManager->getChoicesOfSurvey($idSurvey));
+        
+        reponse(200, "found survey", $survey);
+    } else {
+        reponse(200, "survey not found", NULL);
+    }
+    
+} else {
+    reponse(400, "Invalid Request", NULL);
+}
+
+function reponse($status, $status_message, $data){
+    header ("HTTP/1.1 " . $status);
+    
+    $reponse["status"] = $status;
+    $reponse["status_message"] = $status_message;
+    $reponse["data"] = $data;
+    
+    $json_response = json_encode($reponse);
+    
+    echo $json_response;
+}
